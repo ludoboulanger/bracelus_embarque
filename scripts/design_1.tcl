10430bc06b0b14d9684fb5435230d8732a7a8acb
lboulanger <ludovic.boulanger@outlook.com>
2021-03-22 15:13:27 -0400

    update du tcl

--- scripts/design_1.tcl
+++ scripts/design_1.tcl
@@ -206,6 +206,7 @@ proc create_root_design { parentCell } {
 
   # Create ports
   set i_adc_strobe [ create_bd_port -dir I i_adc_strobe ]
+  set i_bclk_0 [ create_bd_port -dir I i_bclk_0 ]
   set i_data_cardio [ create_bd_port -dir I -from 11 -to 0 i_data_cardio ]
   set i_data_mouvement [ create_bd_port -dir I -from 11 -to 0 i_data_mouvement ]
   set o_cardio_analyse [ create_bd_port -dir O -from 31 -to 0 o_cardio_analyse ]
@@ -718,7 +719,7 @@ proc create_root_design { parentCell } {
   # Create instance: ps7_0_axi_periph, and set properties
   set ps7_0_axi_periph [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 ps7_0_axi_periph ]
   set_property -dict [ list \
-   CONFIG.NUM_MI {6} \
+   CONFIG.NUM_MI {5} \
  ] $ps7_0_axi_periph
 
   # Create instance: rst_ps7_0_50M, and set properties
@@ -742,11 +743,12 @@ proc create_root_design { parentCell } {
   connect_bd_net -net MouvAnalyseIP_0_o_data_out0 [get_bd_ports o_mouv_analyse0] [get_bd_pins MouvAnalyseIP_0/o_data_out0]
   connect_bd_net -net MouvAnalyseIP_0_o_data_out1 [get_bd_ports o_mouv_analyse1] [get_bd_pins MouvAnalyseIP_0/o_data_out1]
   connect_bd_net -net i_adc_strobe_0_1 [get_bd_ports i_adc_strobe] [get_bd_pins MouvAnalyseIP_0/i_adc_strobe]
+  connect_bd_net -net i_bclk_0_1 [get_bd_ports i_bclk_0] [get_bd_pins MouvAnalyseIP_0/i_bclk]
   connect_bd_net -net i_data_echantillon_0_1 [get_bd_ports i_data_mouvement] [get_bd_pins MouvAnalyseIP_0/i_data_echantillon]
   connect_bd_net -net i_data_echantillon_0_2 [get_bd_ports i_data_cardio] [get_bd_pins CardioAnalyseIP_0/i_data_echantillon]
-  connect_bd_net -net processing_system7_0_FCLK_CLK0 [get_bd_pins CardioAnalyseIP_0/s00_axi_aclk] [get_bd_pins MouvAnalyseIP_0/mouvanalyseip_aclk] [get_bd_pins PmodGPIO_0/s_axi_aclk] [get_bd_pins axi_gpio_0/s_axi_aclk] [get_bd_pins axi_gpio_1/s_axi_aclk] [get_bd_pins processing_system7_0/FCLK_CLK0] [get_bd_pins processing_system7_0/M_AXI_GP0_ACLK] [get_bd_pins ps7_0_axi_periph/ACLK] [get_bd_pins ps7_0_axi_periph/M00_ACLK] [get_bd_pins ps7_0_axi_periph/M01_ACLK] [get_bd_pins ps7_0_axi_periph/M02_ACLK] [get_bd_pins ps7_0_axi_periph/M03_ACLK] [get_bd_pins ps7_0_axi_periph/M04_ACLK] [get_bd_pins ps7_0_axi_periph/M05_ACLK] [get_bd_pins ps7_0_axi_periph/S00_ACLK] [get_bd_pins rst_ps7_0_50M/slowest_sync_clk]
+  connect_bd_net -net processing_system7_0_FCLK_CLK0 [get_bd_pins CardioAnalyseIP_0/s00_axi_aclk] [get_bd_pins MouvAnalyseIP_0/mouvanalyseip_aclk] [get_bd_pins PmodGPIO_0/s_axi_aclk] [get_bd_pins axi_gpio_0/s_axi_aclk] [get_bd_pins axi_gpio_1/s_axi_aclk] [get_bd_pins processing_system7_0/FCLK_CLK0] [get_bd_pins processing_system7_0/M_AXI_GP0_ACLK] [get_bd_pins ps7_0_axi_periph/ACLK] [get_bd_pins ps7_0_axi_periph/M00_ACLK] [get_bd_pins ps7_0_axi_periph/M01_ACLK] [get_bd_pins ps7_0_axi_periph/M02_ACLK] [get_bd_pins ps7_0_axi_periph/M03_ACLK] [get_bd_pins ps7_0_axi_periph/M04_ACLK] [get_bd_pins ps7_0_axi_periph/S00_ACLK] [get_bd_pins rst_ps7_0_50M/slowest_sync_clk]
   connect_bd_net -net processing_system7_0_FCLK_RESET0_N [get_bd_pins processing_system7_0/FCLK_RESET0_N] [get_bd_pins rst_ps7_0_50M/ext_reset_in]
-  connect_bd_net -net rst_ps7_0_50M_peripheral_aresetn [get_bd_pins CardioAnalyseIP_0/s00_axi_aresetn] [get_bd_pins MouvAnalyseIP_0/mouvanalyseip_aresetn] [get_bd_pins PmodGPIO_0/s_axi_aresetn] [get_bd_pins axi_gpio_0/s_axi_aresetn] [get_bd_pins axi_gpio_1/s_axi_aresetn] [get_bd_pins ps7_0_axi_periph/ARESETN] [get_bd_pins ps7_0_axi_periph/M00_ARESETN] [get_bd_pins ps7_0_axi_periph/M01_ARESETN] [get_bd_pins ps7_0_axi_periph/M02_ARESETN] [get_bd_pins ps7_0_axi_periph/M03_ARESETN] [get_bd_pins ps7_0_axi_periph/M04_ARESETN] [get_bd_pins ps7_0_axi_periph/M05_ARESETN] [get_bd_pins ps7_0_axi_periph/S00_ARESETN] [get_bd_pins rst_ps7_0_50M/peripheral_aresetn]
+  connect_bd_net -net rst_ps7_0_50M_peripheral_aresetn [get_bd_pins CardioAnalyseIP_0/s00_axi_aresetn] [get_bd_pins MouvAnalyseIP_0/mouvanalyseip_aresetn] [get_bd_pins PmodGPIO_0/s_axi_aresetn] [get_bd_pins axi_gpio_0/s_axi_aresetn] [get_bd_pins axi_gpio_1/s_axi_aresetn] [get_bd_pins ps7_0_axi_periph/ARESETN] [get_bd_pins ps7_0_axi_periph/M00_ARESETN] [get_bd_pins ps7_0_axi_periph/M01_ARESETN] [get_bd_pins ps7_0_axi_periph/M02_ARESETN] [get_bd_pins ps7_0_axi_periph/M03_ARESETN] [get_bd_pins ps7_0_axi_periph/M04_ARESETN] [get_bd_pins ps7_0_axi_periph/S00_ARESETN] [get_bd_pins rst_ps7_0_50M/peripheral_aresetn]
 
   # Create address segments
   assign_bd_address -offset 0x43C10000 -range 0x00010000 -target_address_space [get_bd_addr_spaces processing_system7_0/Data] [get_bd_addr_segs CardioAnalyseIP_0/S00_AXI/S00_AXI_reg] -force
